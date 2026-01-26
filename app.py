fe181505b18f28afbb9da5dc71439924bc119a8c
Luc <njakatsarovan20@gmail.com>
2026-01-26 15:46:01 +0300

    nettoyage de code complet

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import os
-from flask import Flask, render_template_string
+from flask import Flask, render_template_string, request, redirect, url_for
 from redis import Redis
 
 app = Flask(__name__)
+# Connexion à Redis
 db = Redis(host='redis', port=6379, decode_responses=True)
 
-# On récupère le titre depuis l'environnement
+# Variable d'environnement pour le titre
 app_title = os.getenv('APP_TITLE', 'Ma Liste de Tâches')
 
 HTML_TEMPLATE = '''
@@ -30,7 +31,20 @@ HTML_TEMPLATE = '''
 @app.route('/')
 def index():
     tasks = db.lrange('tasks', 0, -1)
-    # On passe bien 'title' et 'tasks' au template
     return render_template_string(HTML_TEMPLATE, tasks=tasks, title=app_title)
 
-# ... (garde tes routes /add et /delete comme elles étaient)
+@app.route('/add', methods=['POST'])
+def add():
+    task = request.form.get('task')
+    if task:
+        db.rpush('tasks', task)
+    return redirect(url_for('index'))
+
+@app.route('/delete/<task>')
+def delete(task):
+    db.lrem('tasks', 1, task)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    # On lance l'app sur le port 5000
+    app.run(host='0.0.0.0', port=5000)
