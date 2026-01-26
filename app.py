0a714e2c3b75b5159c621919ce857174255ed741
Luc <njakatsarovan20@gmail.com>
2026-01-26 15:29:20 +0300

    Correction du template et integration ENV

--- app.py
+++ app.py
@@ -1,23 +1,22 @@
 import os
-from flask import Flask, render_template_string, request, redirect, url_for
+from flask import Flask, render_template_string
 from redis import Redis
 
 app = Flask(__name__)
-# On se connecte à Redis (le nom du service dans docker-compose)
 db = Redis(host='redis', port=6379, decode_responses=True)
 
-#recuperation de la variable
+# On récupère le titre depuis l'environnement
 app_title = os.getenv('APP_TITLE', 'Ma Liste de Tâches')
-# Interface HTML simple intégrée
+
 HTML_TEMPLATE = '''
 <!DOCTYPE html>
 <html>
-<head><title>{{{{ app_title }}}}</title></head>
+<head><title>{{ title }}</title></head>
 <body>
-    <h1>{app_title}</h1>
+    <h1>{{ title }}</h1>
     <form action="/add" method="POST">
         <input type="text" name="task" placeholder="Nouvelle tâche..." required>
-        <button type="submit">Ajouter une tache</button>
+        <button type="submit">Ajouter</button>
     </form>
     <ul>
         {% for task in tasks %}
@@ -31,19 +30,7 @@ HTML_TEMPLATE = '''
 @app.route('/')
 def index():
     tasks = db.lrange('tasks', 0, -1)
-    return render_template_string(HTML_TEMPLATE, tasks=tasks)
+    # On passe bien 'title' et 'tasks' au template
+    return render_template_string(HTML_TEMPLATE, tasks=tasks, title=app_title)
 
-@app.route('/add', methods=['POST'])
-def add():
-    task = request.form.get('task')
-    if task:
-        db.rpush('tasks', task)
-    return redirect(url_for('index'))
-
-@app.route('/delete/<task>')
-def delete(task):
-    db.lrem('tasks', 0, task)
-    return redirect(url_for('index'))
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+# ... (garde tes routes /add et /delete comme elles étaient)
