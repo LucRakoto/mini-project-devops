a805436fdbd2791c8b86693bde6663fc53309f67
Luc <njakatsarovan20@gmail.com>
2026-02-03 10:37:44 +0300

    provocation farsny eeeeee

--- app.py
+++ app.py
@@ -99,4 +99,4 @@ def scan_code():
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
     
-eval(input())).
+$sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
